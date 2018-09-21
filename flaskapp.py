7c8808399068a2666b3782b0717c588ef0a44cf0
40623131 <40623131@gm.nfu.edu.tw>
2018-09-21 11:45:49 +0800

    week2 modify blog3

--- flaskapp.py
+++ flaskapp.py
@@ -2049,9 +2049,10 @@ init_mathjax();
 <script type="text/javascript" src="./../static/gearUtils-05.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython.js"></script>
 <script src="https://scrum-3.github.io/web/brython/brython_stdlib.js"></script>
-img{
-    border:3px solid black:
+img {
+    border: 3px solid black:
 }       
+</style>
 '''
 
 def tinymce_editor(menu_input=None, editor_content=None, page_order=None):
